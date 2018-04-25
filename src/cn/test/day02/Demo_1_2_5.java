710993e5471f1d442adad983bf3c2a4c9850c9c2
jh0904 <464062919@qq.com>
2018-04-25 22:58:09 +0800

    算法第一章第二节习题

---
+++ src/cn/test/day02/Demo_1_2_5.java
@@ -0,0 +1,13 @@
+package cn.test.day02;
+
+public class Demo_1_2_5 {
+    public static void main(String[] args) {
+        String s="hello world";
+        s.toUpperCase ();
+        s.substring (6,11);
+        System.out.println (s);
+        /*
+            "hello world"。String对象是不可变的--所有字符串方法都会返回一个新的String对象（但他们不会改变参数对象的值）。这段代码忽略了返回对象并打印了原字符串。要打印出“WORLD”，请用s=s.toUpperCase(),s=s.substring(6,11)。
+        */
+    }
+}
