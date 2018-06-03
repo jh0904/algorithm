334f1bb3721b5b424ba9055210de8d250a411dc4
jh0904 <464062919@qq.com>
2018-06-03 15:51:55 +0800

    背包

--- src/cn/test1/day03/Bag.java
+++ src/cn/test1/day03/Bag.java
@@ -1,12 +1,38 @@
 package cn.test1.day03;
 
+import java.util.Iterator;
+import java.util.ListIterator;
+
 public class Bag {
     private Node first;
     private class Node{
         String item;
-        Node nest;
+        Node next;
     }
     public void add(String item){
+        Node oldfirst =first;
+        first=new Node ();
+        first.item=item;
+        first.next=oldfirst;
+    }
+    public Iterable<String> iterable(){
+        return new ListIterator ();
+    }
+    private class ListIterator implements Iterable<String>{
+        private Node current=first;
+        public boolean hasNext(){
+            return current!=null;
+        }
+        public void remove(){}
+        public String next(){
+            String item = current.item;
+            current=current.next;
+            return item;
+        }
 
+        @Override
+        public Iterator<String> iterator() {
+            return null;
+        }
     }
 }
