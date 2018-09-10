475ebfd3fb47bb1a1a413db40b1d8aebaa660e46
jh0904 <464062919@qq.com>
2018-09-10 10:19:35 +0800

    增加删除链表重复元素，第一个方法是使用HashMap作为校验，第二个方法是使用双层循环。

--- src/cn/test1/linklist/LinkList.java
+++ src/cn/test1/linklist/LinkList.java
@@ -2,6 +2,7 @@ package cn.test1.linklist;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 /**
@@ -13,8 +14,8 @@ import java.util.LinkedList;
  */
 
 public class LinkList {
-	class Node{
-		Node next=null;
+	class Node {
+		Node next = null;
 		int data;
 
 		public Node(int data) {
@@ -22,54 +23,55 @@ public class LinkList {
 		}
 	}
 
-	Node head=null;
+	Node head = null;
 	//链表头节点的引用
 
 	/**
 	 * 给链表中插入数据
-	 * */
-	public void adddNode(int d){
+	 */
+	public void adddNode(int d) {
 		Node newNode = new Node (d);
 		if (head == null) {
-			head=newNode;
+			head = newNode;
 			return;
 		}
 		Node tmp = this.head;
-		while(tmp.next!=null){
-			tmp=tmp.next;
+		while (tmp.next != null) {
+			tmp = tmp.next;
 		}
 
 		//add node to end
-		tmp.next=newNode;
+		tmp.next = newNode;
 	}
+
 	/**
 	 * 删除指定位置元素
-	 * */
+	 */
 
-	public boolean deleteNode(int index){
-		if(index<1||index>length()){
+	public boolean deleteNode(int index) {
+		if (index < 1 || index > length ()) {
 			return false;
 		}
 
 		//删除第一个元素
-		if(index==1){
-			head=head.next;
+		if (index == 1) {
+			head = head.next;
 			return true;
 		}
 		//删除的是中间元素
-		int i=2;
+		int i = 2;
 		//当前节点的前一个节点
 		Node preNode = this.head;
 		//要删除的节点。
 		Node curNode = preNode.next;
-		while (curNode!=null){
-			if(i==index){
+		while (curNode != null) {
+			if (i == index) {
 				//删除的方式就是跳过当前节点，用上一个节点的next直接指向删除节点的next
-				preNode.next=curNode.next;
+				preNode.next = curNode.next;
 				return true;
 			}
-			preNode=curNode;
-			curNode= curNode.next;
+			preNode = curNode;
+			curNode = curNode.next;
 			i++;
 		}
 		return true;
@@ -77,13 +79,13 @@ public class LinkList {
 
 	/**
 	 * 返回节点长度
-	 * */
-	public int length(){
-		int length=0;
+	 */
+	public int length() {
+		int length = 0;
 		Node tmp = this.head;
-		while (tmp!=null){
+		while (tmp != null) {
 			length++;
-			tmp=tmp.next;
+			tmp = tmp.next;
 		}
 		return length;
 	}
@@ -93,12 +95,50 @@ public class LinkList {
 
 	/**
 	 * 打印链表
-	 * */
-	public void printList(){
+	 */
+	public void printList() {
 		Node tmp = this.head;
-		while (tmp!=null){
+		while (tmp != null) {
 			System.out.println (tmp.data);
-			tmp=tmp.next;
+			tmp = tmp.next;
+		}
+	}
+
+	/**
+	 * 删除链表重复元素
+	 */
+	public void deleteDuplecate(Node head) {
+		HashMap<Integer, Integer> map = new HashMap<> ();
+		Node tmp = head;
+		Node pre = null;
+		while (tmp != null) {
+			if (map.containsKey (tmp.data)) {
+				pre.next = tmp.next;
+			} else {
+				map.put (tmp.data, 1);
+				pre = tmp;
+			}
+			tmp = tmp.next;
+		}
+	}
+	public void deleteDuplecate1(Node head) {
+		/*
+		采用双层循环遍历，外循环正常遍历链表，假设外循环当前结点为cur，内循环就从cur开始遍历，若碰到与cur所指结点相同的值，则删除这个重复结点。
+		 */
+		Node p = head;
+		while (p!=null){
+			Node q=p;
+			while (q.next!=null){
+				//若当前值与下一个值的data相等
+				if(p.data==q.next.data){
+					//跳过下一次的值
+					q.next=q.next.next;
+				}else {
+					//不相等继续向下遍历
+					q=q.next;
+				}
+			}
+			p=p.next;
 		}
 	}
 
@@ -107,8 +147,12 @@ public class LinkList {
 		linkList.adddNode (3);
 		linkList.adddNode (1);
 		linkList.adddNode (5);
+		linkList.adddNode (5);
+		linkList.adddNode (5);
+		linkList.adddNode (5);
 		linkList.adddNode (3);
-		linkList.deleteNode (1);
+		//linkList.deleteNode (1);
+		linkList.deleteDuplecate1 (linkList.head);
 		System.out.println ("length---->" + linkList.length ());
 		linkList.printList ();
 
