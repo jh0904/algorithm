6e9b66cb64f7fcd63ba59236fbdeda0ae0be5628
jh0904 <464062919@qq.com>
2018-09-13 23:55:45 +0800

    更改了一些冗余代码

--- src/cn/test1/c2_sort/Test.java
+++ src/cn/test1/c2_sort/Test.java
@@ -8,26 +8,15 @@ package cn.test1.c2_sort;
  * description:
  */
 class A {
-	public A() {
-		super();
-	}
-
 	public String show(D obj) {
 		return ("A D");
 	}
-	/*public String show(C obj) {
-		return ("A C");
-	}*/
 	public String show(A obj) {
 		return ("A A");
 	}
 }
 
 class B extends A {
-	public B() {
-		super();
-	}
-
 	public String show(B obj) {
 		return ("B B");
 	}
@@ -37,15 +26,9 @@ class B extends A {
 }
 
 class C extends B {
-	public C() {
-		super();
-	}
 }
 
 class D extends B {
-	public D() {
-		super();
-	}
 }
 
 public class Test {
@@ -54,9 +37,9 @@ public class Test {
 		B b = new B ();
 		C c = new C ();
 		D d = new D ();
-		System.out.println (a.show (c));
-		System.out.println (a.show (d));
-		System.out.println (b.show (c));
-		System.out.println (b.show (d));
+		System.out.println (a.show (c));//aa
+		System.out.println (a.show (d));//ad
+		System.out.println (b.show (c));//bb
+		System.out.println (b.show (d));//bb
 	}
 }
